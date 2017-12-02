c7b28456738a602b65f97bb6910617358d1747c5
AndreAvet <andre.abedmamoore@gmail.com>
2017-12-01 20:48:59 -0800

    delete account implemented

--- deleteAccount.php
+++ deleteAccount.php
@@ -1,32 +1,39 @@
 <?php
 
-$db = @mysqli_connect (localhost, "root", "root")
-  Or die("<div class='error' ><p>Could not connect to mysql.<br>Error Code" . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p></div>");
+	$db = @mysqli_connect (localhost, "root", "root")
+	  Or die("<div class='error' ><p>Could not connect to mysql.<br>Error Code" . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p></div>");
 
-@mysqli_select_db($db, "group_c")
-  Or die("<div class='error'><p>Could not connect to database<br>Error Code" . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p></div>");
+	@mysqli_select_db($db, "group_c")
+	  Or die("<div class='error'><p>Could not connect to database<br>Error Code" . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p></div>");
 
-$_POST['dPassword'] = sha1('asjdokasjkdasdnjkanbshdadjnaskdbakhbhabsdkhakjdnakjsndjkasnsad'.$_POST['dPassword']);
-unset($_POST['newPassword2']);
+	$_POST['dPassword'] = sha1('asjdokasjkdasdnjkanbshdadjnaskdbakhbhabsdkhakjdnakjsndjkasnsad'.$_POST['dPassword']);
 
-$res = mysqli_query($db, 'UPDATE users
-SET password = "'.$_POST['newPassword1'].'"
-WHERE username = "'.$_COOKIE['username'].'"
-AND token = "'.$_COOKIE['token'].'"
-AND password = "'.$_POST['oldPassword'].'"'); 
+	$check = mysqli_query($db, 'SELECT username
+	FROM users WHERE password = "'.$_POST['dPassword'].'"
+	AND username = "'.$_COOKIE['username'].'"
+	AND token = "'.$_COOKIE['token'].'"'); 
 
+	if ($check->num_rows == 0) {
+		header("Location: /deleteAccount-fail.php"); /* Redirect browser */
+		exit();
+	}
 
-mysqli_free_result($res);
-mysqli_close($db);
+	$res = mysqli_query($db, 'DELETE FROM users
+	WHERE username = "'.$_COOKIE['username'].'"
+	AND token = "'.$_COOKIE['token'].'"
+	AND password = "'.$_POST['dPassword'].'"'); 
 
-if (isset($_COOKIE['token'])) {
-  setcookie('token', '', time()-3600);
-}
-if (isset($_COOKIE['username'])) {
-  setcookie('username', '', time()-3600);
-}
+	mysqli_free_result($res);
+	mysqli_close($db);
 
-  header("Location: /account.php"); /* Redirect browser */
-  exit();
+	if (isset($_COOKIE['token'])) {
+	  setcookie('token', '', time()-3600);
+	}
+	if (isset($_COOKIE['username'])) {
+	  setcookie('username', '', time()-3600);
+	}
+
+	header("Location: /sorry_to_see_you_go.html"); /* Redirect browser */
+	exit();
 
 ?>
